95f0c1d7181992d545e2e829f9f836324f0e4176
hilina88 <hilinawuhib@gmail.com>
2022-01-28 12:41:31 +0100

    product crud

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import listEndpoints from "express-list-endpoints";
 import cors from "cors";
 import reviewRouter from "./services/CRUD/Reviews/review.js";
+import productsRouter from "./services/CRUD/Products/product.js";
 const server = express();
 
 const port = 3001;
@@ -10,6 +11,7 @@ server.use(cors());
 server.use(express.json());
 
 server.use("/review", reviewRouter);
+server.use("/products", productsRouter);
 
 console.table(listEndpoints(server));
 
