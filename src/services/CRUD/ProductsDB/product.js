44a530b636df60a37f21c5fed81b8516f7b71c00
bokijonovm <muhsinjonboqijonov2002@gmail.com>
2022-02-09 15:22:46 +0100

    products CRUD is working

--- src/services/CRUD/ProductsDB/product.js
+++ src/services/CRUD/ProductsDB/product.js
@@ -14,7 +14,7 @@ productsRouter.get("/", async (req, res, next) => {
 
 productsRouter.get("/:product_id", async (req, res, next) => {
   try {
-    const singleProduct = await Product.findByPk(req.params.id);
+    const singleProduct = await Product.findByPk(req.params.product_id);
     if (singleProduct) {
       res.send(singleProduct);
     } else {
@@ -22,6 +22,47 @@ productsRouter.get("/:product_id", async (req, res, next) => {
     }
   } catch (error) {
     res.status(500).send({ error: error.message });
+  }
+});
+
+//
+
+productsRouter.post("/", async (req, res, next) => {
+  try {
+    const newProduct = await Product.create(req.body);
+    res.send(newProduct);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
+
+productsRouter.put("/:product_id", async (req, res, next) => {
+  try {
+    //
+    const [success, updateProduct] = await Product.update(req.body, {
+      where: { product_id: req.params.product_id },
+      returning: true,
+    });
+    if (success) {
+      res.send(updateProduct);
+    } else {
+      res.status(404).send({ message: "no such product" });
+    }
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
+
+productsRouter.delete("/:product_id", async (req, res, next) => {
+  try {
+    await Product.destroy({
+      where: {
+        product_id: req.params.product_id,
+      },
+    });
+    res.status(204).send();
+  } catch (error) {
+    res.status(500).send({ message: error.message });
   }
 });
 
