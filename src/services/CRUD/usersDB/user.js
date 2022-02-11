25583755eda2f482a41ce75872dfc146fe5c271a
bokijonovm <muhsinjonboqijonov2002@gmail.com>
2022-02-11 14:17:41 +0100

    users CRUD is working

--- src/services/CRUD/usersDB/user.js
+++ src/services/CRUD/usersDB/user.js
@@ -35,4 +35,51 @@ usersRouter.post("/", async (req, res, next) => {
   }
 });
 
+usersRouter.get("/:id", async (req, res, next) => {
+  try {
+    const singleUser = await Users.findOne({
+      where: {
+        id: req.params.id,
+      },
+      include: [Product],
+    });
+    if (singleUser) {
+      res.send(singleUser);
+    } else {
+      res.status(404).send({ message: "No such user" });
+    }
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
+
+usersRouter.put("/:id", async (req, res, next) => {
+  try {
+    const [success, updatedUser] = await Users.update(req.body, {
+      where: { id: req.params.id },
+      returning: true,
+    });
+    if (success) {
+      res.send(updatedUser);
+    } else {
+      res.status(404).send({ message: "no such user" });
+    }
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+usersRouter.delete("/:id", async (req, res, next) => {
+  try {
+    await Users.destroy({
+      where: { id: req.params.id },
+    });
+    res.send(`User with id ${req.params.id} has successfully deleted!`);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
 export default usersRouter;
